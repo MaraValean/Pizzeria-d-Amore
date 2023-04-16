578483d7d5aaa70aa1695e9a737b98474f9baa23
Valean MV Mara Raluca <91742665+917-Valean-Mara@users.noreply.github.com>
2023-04-16 03:23:26 +0300

    Delete constants.ts

--- frontend/src/constants.ts
+++
@@ -1,5 +0,0 @@
-const PROD_BACKEND_API_URL = "/pizzeriaApp";
-const DEV_BACKEND_API_URL ="http://ec2-13-48-196-82.eu-north-1.compute.amazonaws.com/pizzeriaApp";
-
-export const BACKEND_API_URL =
-	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
